283f1366e85298634851d410d60d5595d7f71ddd
Alfaz-2000 <alfazappu74@gmail.com>
2026-07-26 21:19:28 +0530

    feat: add dynamic flight ticket cards with airline logos

---
+++ app/components/Cards.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Cards = () => {
+    return (
+        <div className='min-h-screen bg-[#09283a] text-white flex flex-col justify-center items-center'>
+            <div className="p-10">
+                <h1 className='text-center font-bold text-5xl text-[#dcb657]'>We provide Tourism for Places</h1>
+            </div>
+            <div className="grid grid-cols-3 gap-4">
+                <div className="">
+                    <div className="img">
+                        <Image className='rounded-full' src="/dubai.png" alt='dubai-image' width={350} height={350} loading="eager" />
+                    </div>
+                    <div className="info">
+                        <div className="sub-info flex flex-col gap-4 p-5 justify-center items-center">
+                            <h2 className='font-bold text-4xl'>DUBAI</h2>
+                            <p className='text-lg text-center'>One of the most visted tourist place in world</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="">
+                    <div className="img">
+                        <Image className='rounded-full' src="/dubai.png" alt='dubai-image' width={350} height={350} loading="eager" />
+                    </div>
+                    <div className="info">
+                        <div className="sub-info flex flex-col gap-4 p-5 justify-center items-center">
+                            <h2 className='font-bold text-4xl'>Thailand</h2>
+                            <p className='text-lg text-center'>One of the most visted tourist place in world</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="">
+                    <div className="img">
+                        <Image className='rounded-full' src="/dubai.png" alt='dubai-image' width={350} height={350} loading="eager" />
+                    </div>
+                    <div className="info">
+                        <div className="sub-info flex flex-col gap-4 p-5 justify-center items-center">
+                            <h2 className='font-bold text-4xl'>Vietnam</h2>
+                            <p className='text-lg text-center'>One of the most visted tourist place in world</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="">
+                    <div className="img">
+                        <Image className='rounded-full' src="/dubai.png" alt='dubai-image' width={350} height={350} loading="eager" />
+                    </div>
+                    <div className="info">
+                        <div className="sub-info flex flex-col gap-4 p-5 justify-center items-center">
+                            <h2 className='font-bold text-4xl'>Kerala</h2>
+                            <p className='text-lg text-center'>One of the most visted tourist place in world</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="">
+                    <div className="img">
+                        <Image className='rounded-full' src="/dubai.png" alt='dubai-image' width={350} height={350} loading="eager" />
+                    </div>
+                    <div className="info">
+                        <div className="sub-info flex flex-col gap-4 p-5 justify-center items-center">
+                            <h2 className='font-bold text-4xl'>Kashmir</h2>
+                            <p className='text-lg text-center'>One of the most visted tourist place in world</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="">
+                    <div className="img">
+                        <Image className='rounded-full' src="/dubai.png" alt='dubai-image' width={350} height={350} loading="eager" />
+                    </div>
+                    <div className="info">
+                        <div className="sub-info flex flex-col gap-4 p-5 justify-center items-center">
+                            <h2 className='font-bold text-4xl'>Rajasthan</h2>
+                            <p className='text-lg text-center'>One of the most visted tourist place in world</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
---
+++ app/components/Visa.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Visa = () => {
+    const visas = [
+  "Umrah Visa / Multiple Visa",
+  "Dubai 30 Days / 60 Days Visa",
+  "Schengen Visa",
+  "Turkey Visa",
+  "Vietnam Visa",
+  "Kenya Visa",
+  "Morocco Visa",
+  "Armenia Visa",
+  "Australia Visa",
+  "USA Visa",
+  "Saudi Arabia GCC Residence Visa",
+  "Bahrain Visa",
+  "Uzbekistan Visa",
+  "Azerbaijan Visa",
+  "Qatar Visa",
+  "Russia Visa",
+  "India Visa",
+  "Zambia Visa",
+  "Bangladesh Visa",
+  "Djibouti Visa",
+  "Egypt Visa",
+  "Georgia Visa",
+  "Hong Kong Visa",
+  "Jordan Visa",
+  "Kyrgyzstan Visa",
+  "Sri Lanka Visa",
+];
+    return (
+        <div className="min-h-[70vh] ">
+            
+
+            <div className="text-[#dcb657] text-4xl font-bold text-center p-5">We help you with Visa</div>
+        <div className='flex flex-col justify-center items-center'>
+            <div className="grid grid-cols-3 gap-5">
+              {visas.map((item)=>(
+                <span key={item} className='bg-[#10384d] text-white rounded-full text-center text-lg p-2'>{item}</span>
+              ))}  
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default Visa
